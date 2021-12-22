4fac36bb0ff2bca7e2db0ab775083314f3834f0b
ranjith <ranjithkumarcena@gmail.com>
2021-12-21 22:38:45 -0500

    origin

---
+++ app/src/main/java/com/restapi/insta/OpenActivity.java
@@ -0,0 +1,91 @@
+package com.restapi.insta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class OpenActivity extends AppCompatActivity {
+
+    private ImageView iconImage;
+    private LinearLayout linearLayout;
+    private Button login, register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open);
+
+
+        iconImage = findViewById(R.id.iconImage);
+        linearLayout = findViewById(R.id.linearLayout);
+        login = findViewById(R.id.loginBt);
+        register = findViewById(R.id.registerBt);
+
+        linearLayout.animate().alpha(0f).setDuration(1);
+
+        TranslateAnimation animation = new TranslateAnimation(0,0,0,-1000);
+        animation.setDuration(1000);
+        animation.setFillAfter(false);
+        animation.setAnimationListener( new MyAnimationListener());
+
+        iconImage.setAnimation(animation);
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(OpenActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(OpenActivity.this, RegisterActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+    }
+
+    private class MyAnimationListener implements Animation.AnimationListener{
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+            iconImage.clearAnimation();
+            iconImage.setVisibility(View.INVISIBLE);
+            linearLayout.animate().alpha(1f).setDuration(500);
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/restapi/insta/RegisterActivity.java
@@ -0,0 +1,134 @@
+package com.restapi.insta;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText username, name, email, password;
+    private Button register;
+    private TextView loginUser;
+
+
+    private DatabaseReference mRootref;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        username = findViewById(R.id.registerUsername);
+        name = findViewById(R.id.registerName);
+        email = findViewById(R.id.registerEmail);
+        password = findViewById(R.id.registerPassword);
+
+        register = findViewById(R.id.registerButton);
+        loginUser = findViewById(R.id.loginUser);
+
+        mRootref = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        loginUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txtUsername = username.getText().toString();
+                String txtName = name.getText().toString();
+                String txtEmail = email.getText().toString();
+                String txtPassword = password.getText().toString();
+
+                if (TextUtils.isEmpty(txtUsername)|| TextUtils.isEmpty(txtName)
+                        ||TextUtils.isEmpty(txtEmail)|| TextUtils.isEmpty(txtPassword)){
+                    Toast.makeText(getApplicationContext(), "Empty credentials", Toast.LENGTH_SHORT).show();
+
+                }else if(txtPassword.length()<6){
+
+                    Toast.makeText(RegisterActivity.this, "Password too short!!", Toast.LENGTH_SHORT).show();
+                }else {
+
+                    registerUser(txtUsername, txtName, txtEmail, txtPassword);
+                }
+
+            }
+        });
+
+
+
+
+    }
+
+    private void registerUser(String username, String name, String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("username", username);
+                map.put("name", name);
+                map.put("email", email);
+                map.put("id", mAuth.getCurrentUser().getUid());
+
+                //add to DB
+                mRootref.child("Users").child(mAuth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "DB updated", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity( intent);
+                            finish();
+                        }
+
+                    }
+                });
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+
+
+}
