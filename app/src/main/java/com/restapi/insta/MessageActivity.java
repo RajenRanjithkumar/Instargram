5846c5c82190f5abcb8c820777cbc06f64a3fb4a
ranjith <ranjithkumarcena@gmail.com>
2022-05-14 19:57:13 -0400

    second search activity added

--- app/src/main/java/com/restapi/insta/MessageActivity.java
+++ app/src/main/java/com/restapi/insta/MessageActivity.java
@@ -2,13 +2,17 @@ package com.restapi.insta;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MessageActivity extends AppCompatActivity {
 
 
+    CardView searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,18 @@ public class MessageActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+
+        searchBar = findViewById(R.id.searchCardView);
+        searchBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+
             }
         });
 
---
+++ app/src/main/java/com/restapi/insta/SearchActivity.java
@@ -0,0 +1,37 @@
+package com.restapi.insta;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        Toolbar toolbar = findViewById(R.id.searchToolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+
+        });
+
+
+        EditText editText = findViewById(R.id.searchEditText);
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+
+    }
+}
