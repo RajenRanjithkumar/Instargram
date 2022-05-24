93deeb3ebd6e747ecc1242cae412262f32c0eb3c
ranjith <ranjithkumarcena@gmail.com>
2022-05-24 18:22:12 -0400

    display users chat done.
    Todo
    online/offline feature

--- app/src/main/java/com/restapi/insta/Adapter/UsersChatDisplayAdapter.java
+++ app/src/main/java/com/restapi/insta/Adapter/UsersChatDisplayAdapter.java
@@ -1,6 +1,7 @@
 package com.restapi.insta.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.restapi.insta.ChatActivity;
+import com.restapi.insta.Model.Chat;
 import com.restapi.insta.Model.User;
 import com.restapi.insta.R;
 import com.squareup.picasso.Picasso;
@@ -21,6 +31,7 @@ public class UsersChatDisplayAdapter extends RecyclerView.Adapter<UsersChatDispl
 
     private Context mContext;
     private List<User> mUsers;
+    private String Lastmsg;
 
     public UsersChatDisplayAdapter(Context mContext, List<User> mUsers) {
         this.mContext = mContext;
@@ -42,8 +53,72 @@ public class UsersChatDisplayAdapter extends RecyclerView.Adapter<UsersChatDispl
         User user = mUsers.get(position);
 
         holder.username.setText(user.getUsername());
+        holder.lastMessage.setText("Last msg");
         Picasso.get().load(user.getImageurl()).placeholder(R.mipmap.ic_launcher).into(holder.imageProfile);
 
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(mContext, ChatActivity.class);
+                intent.putExtra("userId", user.getId());
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
+
+            }
+        });
+
+        getLastMessage(user.getId(), holder.lastMessage);
+
+
+
+
+    }
+
+    private void getLastMessage(String chatUserId, TextView lastMessage) {
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Chats");
+
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+
+                    Chat chat = dataSnapshot.getValue(Chat.class);
+
+                    assert firebaseUser != null;
+                    if (!firebaseUser.getUid().isEmpty() && !chat.getMessage().isEmpty()){
+
+                        if (chat.getReceiver().equals(firebaseUser.getUid()) && chat.getSender().equals(chatUserId)
+                        || chat.getSender().equals(firebaseUser.getUid()) && chat.getReceiver().equals(chatUserId)){
+
+
+                            Lastmsg = chat.getMessage();
+
+
+                        }
+
+
+                        }
+
+                }
+                lastMessage.setText(Lastmsg);
+
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 
